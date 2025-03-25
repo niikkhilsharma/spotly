12902dc9cf4ec1ae6446cd6cd8eca625fcbed02b
shivani <shivani11guleria@gmail.com>
2025-03-25 22:39:57 +0530

    lisitngs

--- app/page.tsx
+++ app/page.tsx
@@ -1,19 +1,21 @@
-import React from 'react'
-import Footer from '@/components/homepage/footer'
-import FAQ from '@/components/homepage/faq'
-import Features from '@/components/homepage/features'
-import Hero from '@/components/homepage/hero'
-import MaxWidthWrapper from '@/components/homepage/wrapper'
-import Sell from '@/components/homepage/sell'
+import React from "react";
+import Footer from "@/components/homepage/footer";
+import FAQ from "@/components/homepage/faq";
+import Features from "@/components/homepage/features";
+import Hero from "@/components/homepage/hero";
+import MaxWidthWrapper from "@/components/homepage/wrapper";
+import Sell from "@/components/homepage/sell";
+import Listings from "@/components/homepage/listings";
 
 export default function Home() {
-	return (
-		<MaxWidthWrapper>
-			<Hero />
-			<Features />
-			<Sell />
-			<FAQ />
-			<Footer />
-		</MaxWidthWrapper>
-	)
+  return (
+    <MaxWidthWrapper>
+      <Hero />
+      <Features />
+      <Sell />
+      <Listings />
+      <FAQ />
+      <Footer />
+    </MaxWidthWrapper>
+  );
 }
---
+++ components/homepage/card.tsx
@@ -0,0 +1,55 @@
+import Image from "next/image";
+
+export default function Card({
+  imageUrl,
+  status,
+  title,
+  price,
+  location,
+  bedrooms,
+}: {
+  imageUrl: string;
+  status: string;
+  title: string;
+  price: string;
+  location: string;
+  bedrooms: string;
+}) {
+  return (
+    <div className="rounded-2xl p-4 overflow-hidden shadow-md bg-white">
+      {/* Image Section */}
+      <div className="relative rounded-2xl  w-full">
+        <Image
+          src={imageUrl}
+          alt={title}
+          width={500}
+          height={500}
+          objectFit="cover"
+          className="rounded-2xl object-cover aspect-square"
+        />
+        <div className="absolute top-4 left-4 bg-white text-black text-sm px-3 py-1 rounded-full shadow">
+          {status}
+        </div>
+      </div>
+
+      {/* Info Section */}
+      <div className="p-4 mt-3">
+        {/* Property Info */}
+        <div className="flex items-center justify-between text-gray-500 text-sm mb-2">
+          <div className="flex items-center gap-1">🛏️ {bedrooms}</div>
+          {/* <div className="flex items-center gap-1">📏 {size}</div> */}
+        </div>
+
+        {/* Title */}
+        <h2 className="text-lg font-semibold mb-1">{title}</h2>
+
+        {/* Price and Location */}
+        <div className="flex items-center gap-2 text-gray-700">
+          <span className="text-black font-bold">{price}</span>
+          <span>•</span>
+          <span>{location}</span>
+        </div>
+      </div>
+    </div>
+  );
+}
